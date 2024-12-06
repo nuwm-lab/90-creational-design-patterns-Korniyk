79481f87bbbceb6820f4754fc7dfecefef46fbc4
Michael Korniichuk <misha17092006@gmail.com>
2024-12-06 23:25:10 +0200

    Added Lab9 in main

--- Program.cs
+++ Program.cs
@@ -1,18 +1,110 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace LabWork
+// Клас космічної ракети
+public class SpaceRocket
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    public List<string> Stages { get; set; } = new();
+    public string EngineType { get; set; }
+    public string Payload { get; set; }
+
+    public override string ToString()
     {
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Hello World!");
-        }
+        return $"Конфігурація ракети:\n" +
+               $"- Ступені: {string.Join(", ", Stages)}\n" +
+               $"- Тип двигуна: {EngineType}\n" +
+               $"- Корисний вантаж: {Payload}";
+    }
+}
+
+// Інтерфейс будівельника
+public interface IRocketBuilder
+{
+    void Reset();
+    void SetStages(List<string> stages);
+    void SetEngineType(string engineType);
+    void SetPayload(string payload);
+    SpaceRocket GetResult();
+}
+
+// Конкретний будівельник
+public class RocketBuilder : IRocketBuilder
+{
+    private SpaceRocket _rocket;
+
+    public RocketBuilder()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        _rocket = new SpaceRocket();
+    }
+
+    public void SetStages(List<string> stages)
+    {
+        _rocket.Stages = stages;
+    }
+
+    public void SetEngineType(string engineType)
+    {
+        _rocket.EngineType = engineType;
+    }
+
+    public void SetPayload(string payload)
+    {
+        _rocket.Payload = payload;
+    }
+
+    public SpaceRocket GetResult()
+    {
+        return _rocket;
+    }
+}
+
+// Директор для створення ракет
+public class RocketDirector
+{
+    private IRocketBuilder _builder;
+
+    public RocketDirector(IRocketBuilder builder)
+    {
+        _builder = builder;
+    }
+
+    public void BuildSimpleRocket()
+    {
+        _builder.Reset();
+        _builder.SetStages(new List<string> { "Ступінь 1" });
+        _builder.SetEngineType("Твердопаливний двигун");
+        _builder.SetPayload("Супутник");
+    }
+
+    public void BuildAdvancedRocket()
+    {
+        _builder.Reset();
+        _builder.SetStages(new List<string> { "Ступінь 1", "Ступінь 2", "Ступінь 3" });
+        _builder.SetEngineType("Рідиннопаливний двигун");
+        _builder.SetPayload("Модуль з екіпажем");
+    }
+}
+
+// Тестування
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        var builder = new RocketBuilder();
+        var director = new RocketDirector(builder);
+
+        Console.WriteLine("Створення простої ракети:");
+        director.BuildSimpleRocket();
+        Console.WriteLine(builder.GetResult());
+
+        Console.WriteLine("\nСтворення складної ракети:");
+        director.BuildAdvancedRocket();
+        Console.WriteLine(builder.GetResult());
     }
 }
